a7931144adba5a83187c9c2567a8fbdac1d589b5
Rasik Abdullah <rasikabdullah@gmail.com>
2023-05-23 02:28:17 +0600

    login and register page added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
-            <h3>Header</h3>
+            <div className="navbar bg-primary text-primary-content">
+                <a className="btn btn-ghost normal-case text-xl">AuthMaster</a>
+                <Link className="btn btn-ghost normal-case text-xl" to="/">Home</Link>
+                <Link className="btn btn-ghost normal-case text-xl" to="/login">Login</Link>
+                <Link className="btn btn-ghost normal-case text-xl" to="/register">Register</Link>
+            </div>
         </div>
     );
 };
