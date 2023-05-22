5e140c3fca7b7503998aee7d02e30427cebda58b
Rasik Abdullah <rasikabdullah@gmail.com>
2023-05-22 23:57:29 +0600

    daisy ui added

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,29 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Main from './layout/Main';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: "/login",
+        element: <Login></Login>
+      },
+      {
+        path: "/register",
+        element: <Register></Register>
+      },
+    ]
   },
 ]);
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,15 @@ export default {
     extend: {},
   },
   plugins: [require("daisyui")],
-
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "light",
+  },
 }
 
