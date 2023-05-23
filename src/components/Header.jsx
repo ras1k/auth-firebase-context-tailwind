25dbc7a5f3e3f9dd70ec2ecd51dbbd359bcd0595
Rasik Abdullah <rasikabdullah@gmail.com>
2023-05-24 02:10:01 +0600

    navigation added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,6 +16,7 @@ const Header = () => {
             <div className="navbar bg-primary text-primary-content">
                 <Link to="/" className="btn btn-ghost normal-case text-xl">AuthMaster</Link>
                 <Link className="btn btn-ghost normal-case text-xl" to="/">Home</Link>
+                <Link className="btn btn-ghost normal-case text-xl" to="/orders">Orders</Link>
                 <Link className="btn btn-ghost normal-case text-xl" to="/login">Login</Link>
                 <Link className="btn btn-ghost normal-case text-xl" to="/register">Register</Link>
                 {
